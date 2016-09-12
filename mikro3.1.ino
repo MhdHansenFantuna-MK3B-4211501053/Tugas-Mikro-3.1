445d6b4413ef171e3ed15e5b10aba65d2e954dc2
MhdHansenFantuna-MK3B-4211501053 <hansenfantuna@gmail.com>
2016-09-12 09:50:47 +0700

    Add files via upload

---
+++ mikro3.1.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+DDRB=0b1111; 
+DDRC=0b0000; 
+PORTC=0b1111; 
+
+}
+
+void loop() {
+   if(digitalRead(A0)==LOW)      
+   {  
+   PORTB = PORTB + 1; 
+    delay(1000); 
+   } 
+    
+   if(digitalRead(A1)==LOW) 
+     { 
+       PORTB = PORTB - 1; 
+       delay(1000); 
+     } 
+    
+   if(digitalRead(A2)==LOW) 
+     { 
+       PORTB=0; 
+     } 
+}
